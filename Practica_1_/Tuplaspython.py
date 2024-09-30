bdd77497054d5b43f7645821fa3c4c0aa2410206
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-09-30 15:25:12 -0600

    Add files via upload

---
+++ Practica_1_/Tuplaspython.py
@@ -0,0 +1,37 @@
+#Creartupla
+thistuple=("manzana, cereza, platano")
+print(thistuple)
+
+#Tuplas con miembros duplicados
+thistuple2=("manzana, platano, cereza, manzana, cereza")
+print(thistuple2)
+
+#Contando miembros de la tupla
+thistuple3=("manzana, platano, cereza")
+print(len(thistuple3))
+
+#Diferencia cuando es Tupla y cuando NO es 
+thistuple4=("Manzana,")
+print(type(thistuple4))
+
+#No es tupla
+thistuple5=("ajo,")
+print(type(thistuple5))
+
+#Asignando tuplas a variables
+tuple1=("manzana, platano, cereza")
+tuple2=(1, 5, 7, 9, 3)
+tuple3=(True, False, False)
+
+#Combinando tipos de datos en mis tuplas
+tuplea1=("abc", 34, True, 40, "male")
+print(tuplea1)
+
+#Tipo de datos de una tupla
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+
+#Utilizando el método tuple para hacer una 
+thistuple = tuple(("apple", "banana", "cherry"))
+print(thistuple)
